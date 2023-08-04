e610edfa14273b77b02bb0c9acd0cec51f1b29d7
bouquet1 <bukuleta@gmail.com>
2023-08-03 19:05:55 -0700

    test passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,7 +1,20 @@
 # your code goes here!
+
+input_list = []
 class Anagram:
     def __init__(self, word):
-        pass
+        self.word = word
 
-    def match():
-        pass
+    def match(self, input_list):
+        anagram = []  
+        #iterate over each word in the input list
+        for input_word in input_list:
+            # check if sorted words match
+            if sorted(self.word) == sorted(input_word):
+                anagram.append(input_word)
+        return anagram
+
+
+
+
+#sorted in python returns a sorted list of the specified iterable object. https://www.w3schools.com/python/ref_func_sorted.asp
